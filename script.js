462e979fabd40bf1f68e1271aa6df3a27be98ec9
ReinisRu <rr@itfly.lv>
2021-01-06 14:52:30 +0200

    JS Toggle active Navigation added, Style adjusted

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".dropdown").addEventListener("click", function () {
+  //Pievieno un noņem klasi `active`
+  document.querySelector(`.sidebar-column`).classList.toggle(`active`);
+});
